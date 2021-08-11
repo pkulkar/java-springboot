c108fc3b27b1912a755170ea38fb88908a43028c
Pranit Madhav Kulkarni <pkulkar2@binghamton.edu>
2021-08-10 23:07:37 -0500

    Basics
    Create hello world

---
+++ restful-web-services/restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	@GetMapping(path="/helloWorld")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
